1f4372c89eccbd4fca2f331cd127f0eecc0772f7
Costanilla <lrcostanilla@paterostechnologicalcollege.edu.ph>
2025-04-13 08:11:41 +0800

    activity1.java

--- activity1.java
+++ activity1.java
@@ -25,6 +25,7 @@ public class StudentNames {
         System.out.println("Heart Janelle D. Serrano - BSIT 2D");
         System.out.println("Sandara L. Panis - BSIT 2D");
         System.out.println("Eisen Dichoso - BSIT 2D");
+	System.out.println("Lanz Rovic Costanilla - BSIT 2C");
         System.out.println("Heart Janelle D. Serrano - BSIT 2D");
         System.out.println("Gabrielle Daniela P. Miranda - BSIT 2C");
         System.out.println("Leonardo M. Sayas - BSIT 2D");
